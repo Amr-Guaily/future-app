edd41f0d52ef4284fb9bf4b208dc023e3148af23
Amr Guaily <amrguaily@gmail.com>
2023-08-22 12:22:48 +0300

    First submit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,jsx,tsx,mdx}",
+    "./pages/**/*.{js,jsx,tsx,mdx}",
+    "./components/**/*.{js,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
