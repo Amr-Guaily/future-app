2b03282a9cf7b8c1fee87eb0c1e2bc058a66f78d
Amr Guaily <amrguaily@gmail.com>
2023-08-29 00:02:32 +0300

    scroll-to-top while navigating

--- pages/_app.js
+++ pages/_app.js
@@ -4,8 +4,20 @@ import AOSWrapper from '../components/AOSWrapper';
 
 import 'aos/dist/aos.css';
 import '../styles/globals.css';
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter();
+
+  useEffect(() => {
+    const scrollToTop = () => window.scrollTo(0, 0);
+
+    router.events.on('routeChangeComplete', scrollToTop);
+
+    return () => router.events.off('hashChangeComplete', scrollToTop);
+  }, [router]);
+
   return (
     <TranslateProvidor>
       <Layout>
